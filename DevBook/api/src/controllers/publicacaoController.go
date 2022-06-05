a73c40399f523cf2ad0f8e6ad4d62220b0fad727
Tarcísio da Silva <tarcisio.dasilva@osf.digital>
2022-06-05 11:23:30 -0300

    creating rotas and creating controller for publicacao

---
+++ DevBook/api/src/controllers/publicacaoController.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+//CriarPublicacao é responsável por salvar uma publicação no banco de dados
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//BuscarPublicacoes é resposável por retornar todas as publicações de um determinado usuário
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//BuscarPublicacao é resposável por retornar uma publicação em particular
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//AtualizarPublicacao é responsável por atualizar uma publicação em particular
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//DeletarPublicacao é responsável por deletar uma publicação em particular
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
